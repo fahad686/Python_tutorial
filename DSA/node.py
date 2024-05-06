b38acaaf1da56da769613a6d5904ef881b0d0025
Fahad Ali <fahad.ali@schuja.com>
2024-05-06 19:03:25 +0500

    delete linked list from beginning, end and with matching value

--- DSA/node.py
+++ DSA/node.py
@@ -64,6 +64,41 @@ class LinkedList:
 
 
 
+            #---------------------------------------------- Deleting node -------------------------------------------------------------#
+
+
+            # Deleting the First Node (Head)
+            # When deleting the head, you want to update the head pointer to point to the next node, effectively removing the first node from the list.
+            # Check if List is Empty: If self.head is None, the list is empty, and there's nothing to delete.
+            # Move the Head Pointer: If there's at least one node, update the head pointer to the next node (self.head.next). This removes the first node from the list.
+
+
+    def delete_at_beginning(self):
+        if self.head is None:
+            print("List is already empty")
+        else:
+         self.head=self.head.next  # Storing head address to the next node
+
+
+
+        # Deleting the Last Node
+        # To delete the last node, you need to traverse the list to find the second-to-last node, then set its next pointer to None. This breaks the link to the last node, effectively deleting it.Check if List is Empty: If self.head is None, the list is already empty.
+        # Check if Only One Node: If self.head.next is None, it means there's only one node. Setting self.head to None deletes the entire list.
+        # Traverse to Second-to-Last Node: You traverse the list, stopping at the second-to-last node by checking current.next.next. If current.next.next is None, you're at the second-to-last node.
+        # Break the Link to the Last Node: Set the next pointer of the second-to-last node to None, removing the last node from the list.
+
+    def delete_at_end(self):
+       if self.head is None:
+           print("List is already empty")
+       elif self.head.next is None:
+           self.head=None #if the list have only one node so delete the entire list
+       else:
+         current =self.head
+         while current.next.next: # Traverse the list 2nd node  to last node
+             current=current.next
+         current.next=None # Break the link to the last node
+
+
 
 
 
@@ -73,5 +108,9 @@ lkd =LinkedList()
 lkd.insert_at_beginning(10)
 lkd.insert_at_beginning(8)
 lkd.insert_at_beginning(6)
+lkd.insert_at_beginning(4)
+lkd.delete_at_end()
+lkd.insert_at_beginning(2)
+
 
 lkd.displaying()
