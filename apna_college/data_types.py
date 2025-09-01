e6e3f2175193b42ecf09c82c0f9117961a93a673
Fahad Ali <fahadofficial686@gmail.com>
2025-09-01 20:37:29 +0500

    type casting and some basics

---
+++ apna_college/data_types.py
@@ -0,0 +1,26 @@
+# auto type cast 
+x=67746_3333
+print(type(x))
+print(x)
+#if i want to type cast it automatically in boolean
+y = float(x)
+print(type(y))
+print(y)
+
+# String to int
+age='27  '
+print(type(age))
+print(age)
+
+print(float(age))
+
+
+
+#complex number ( A combination of real and imaginary numbers)
+z = 3 + 5j
+print(type(z))
+print(z)
+
+#power of 2**2 (need double **)
+a=2
+print(a**3)
--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 # First Program of  3 hours course code with harry
-print("I love Python")
-  
+# print("I love Python")
+# print('Test 1 2 3')
+
+
+#take input from user
+# name=input("Enter your name: ")
+# print("Hello " + name)
+
+#Type casting input
+# age=int(input("Enter your age: "))
+# print("You are " + str(age) + " years old.")
+
+marks=10
+marksPoints=float(marks)
+print('type of marks'+ str(marksPoints))
