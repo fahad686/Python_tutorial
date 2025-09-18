7124619f20121ba4022c8633798bb720e209a1e9
Fahad Ali <fahadofficial686@gmail.com>
2025-09-19 03:27:50 +0500

    data types ( list, dict, tuple, complex, float, set, byte, range,frozenset etc)

--- data type/basic_data_types.py
+++ data type/basic_data_types.py
@@ -49,3 +49,64 @@ print(c1 + c2)  # Addition: (4-2j)
 print(c1 - c2)  # Subtraction: (-2+6j)
 print(c1 * c2)  # Multiplication: (11+2j)
 print(c1 / c2)  # Division: (-0.2+0.4j)
+
+
+######### list
+
+x = ["apple", "banana", "cherry"]
+print(x)
+print(type(x))
+
+
+#############
+x = ("apple", "mango", "cherry")
+print(x)
+print(type(x))
+
+
+########## range  its  start from 0 ....
+
+x_range=range(10)
+print(x_range)
+print(type(x_range))
+
+########### dic its stor value as  map like key value
+
+
+x = {"name" : "John", "age" : 36}
+print(x)
+print(type(x))
+
+
+######## set
+x = {"apple", "banana", "cherry"}
+print(x)
+print(type(x))
+
+
+######### frozenset
+x = frozenset(("apple", "banana", "cherry")) ##	frozenset
+print(x)
+print(type(x))
+
+##### bytes
+x = b"Hello"
+v=bytes(5)
+print(v)
+print(x)
+
+############## bytearray
+x = bytearray(5)
+print(x)
+
+
+########### memoryview
+x = memoryview(bytes(5))
+print(x)
+
+
+###### None
+x = None
+print(x)
+x=3
+print(x)
